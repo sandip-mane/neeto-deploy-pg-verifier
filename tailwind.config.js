64367cbdcbf811dd1cfac0e5901caa054671a936
Sandip Mane <sandip2490@gmail.com>
2024-07-19 21:55:21 +0530

    Copied wheel

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  important: true,
+  content: [
+    "./app/javascript/**/*.js",
+    "./app/javascript/**/**/*.js",
+    "./app/javascript/**/**/**/*.js",
+    "./app/javascript/**/*.jsx",
+    "./app/javascript/**/**/*.jsx",
+    "./app/javascript/**/**/**/*.jsx",
+    "./app/views/**/**/*.slim",
+    "./app/views/**/**/*/*.slim",
+    "./node_modules/@bigbinary/neetoui/**/*",
+  ],
+};
